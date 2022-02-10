7fc554b9372429797f900f0d6c9854205d204936
Narresx <92748159+Narresx@users.noreply.github.com>
2022-02-10 17:18:04 +0100

    fix card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,22 @@
 <template>
   <main class="background-main">
     <section class="container w-80">
-      <div class="row">
-        <div v-for="(album, index) in albums" :key="index" class="col-2">
-          {{ album.response.title }}
+      <div class="row py-5">
+        <div v-for="(album, index) in albums" :key="index" class="col w-25">
+          <div class="card d-flex justify-content-center background-card my-2">
+            <img :src="album.poster" alt="" />
+            <div class="container text-center">
+              <h2 class="text-white p-2">
+                {{ album.title }}
+              </h2>
+              <div class="text-muted">
+                {{ album.author }}
+              </div>
+              <div class="text-muted">
+                {{ album.year }}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -25,7 +38,7 @@ export default {
     axios
       .get("https://flynn.boolean.careers/exercises/api/array/music")
       .then((res) => {
-        this.albums = res.data.results;
+        this.albums = res.data.response;
       });
   },
 };
@@ -34,5 +47,18 @@ export default {
 <style scoped>
 .background-main {
   background-color: rgb(30, 45, 59);
+}
+
+.card img {
+  height: 205px;
+  padding: 10px;
+}
+
+.background-card {
+  background-color: rgb(46, 58, 70);
+}
+
+h2 {
+  font-size: 0.9rem;
 }
 </style>
