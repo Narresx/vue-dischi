3b007a643cb8b52f6b16560987e31b06f10a9a40
Narresx <92748159+Narresx@users.noreply.github.com>
2022-02-11 15:22:43 +0100

    add select

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,19 @@
 <template>
-  <header class="p-3">
+  <header class="p-3 d-flex justify-content-around align-items-center">
     <img
       src="https://www.nicepng.com/png/full/811-8110513_spotify-logo-spotify.png"
     />
+    <Search />
   </header>
 </template>
 
 <script>
+import Search from "./Search.vue";
 export default {
   name: "Header",
+  components: {
+    Search,
+  },
 };
 </script>
 
---
+++ src/components/Search.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex">
+    <select class="form-select" id="music-filter">
+      <option value="1">Rock</option>
+      <option value="2">Pop</option>
+      <option value="3">Metal</option>
+      <option value="4">Jazz</option>
+    </select>
+    <button type="button" class="btn btn-success">Conferma</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+};
+</script>
+
+<style>
+</style>
